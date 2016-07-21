d834b67459c8c71cc9498a51d6b46634108ec511
Jake Willingham <jakewillingham789@gmail.com>
2016-07-21 16:20:12 -0400

    updated the timeline page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+fetch('http://thinksaydo.com/tiyproxy.php?url=')
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
+    json.results.forEach(function(result){
+      
+    })
+  })
